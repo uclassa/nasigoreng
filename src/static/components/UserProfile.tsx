0c69f4a9c74fcd413270c85ace63faa483083117
tianyu139 <liutianyu100@gmail.com>
2019-10-01 13:05:29 -0700

    Added change major and preffered comm in profile

--- src/static/components/UserProfile.tsx
+++ src/static/components/UserProfile.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { ChangeEvent } from "react";
 import axios from "axios";
 import { Table, Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
 import { ISimpleUser } from "../../models/User";
@@ -19,10 +20,12 @@ interface IUserProfileState {
 class UserProfilePage extends React.Component<IUserProfileProps, IUserProfileState> {
   constructor(props) {
     super(props);
+    // TODO: Change hacky method for setting user data
     this.state = {
       userData: this.props.appState.userData
     };
     this.handleChange = this.handleChange.bind(this)
+    this.handleSave = this.handleSave.bind(this)
   }
 
   componentDidMount() {
@@ -30,57 +33,45 @@ class UserProfilePage extends React.Component<IUserProfileProps, IUserProfileSta
   }
 
   reload() {
-    this.setState({ userData: this.props.appState.userData });
+    if (!this.state.userData) return;
+    const email = this.state.userData.email;
+    axios.get<ISingleUserResponse>("/api/users/current").then(data => {
+      this.setState({ userData: data.data.user });
+    });
   }
 
-  handleChange(event) {
-    console.log("Handing some changes");
-    console.log(event)
+  // TODO: Add input validation
+  handleChange = (key: string) => (event: ChangeEvent) => {
     if (!this.state.userData) return;
-    const target = event.target;
+    const target = event.target as HTMLInputElement;
     const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
     const email = this.state.userData.email;
 
+    const newState = {
+      userData: {
+        ...this.state.userData,
+        [key]: value
+      }
+    };
+
+    this.setState(newState);
+  };
+
+  handleSave(event) {
+    if (!this.state.userData) return;
+    const email = this.state.userData.email;
     axios
       .put<ISingleUserResponse>(`/api/users/${encodeURIComponent(email)}`, {
         ...this.state.userData,
-        pcpMentor: value
       })
       .then(result => {
-        this.state.userData.pcpMentor = value;
         this.reload();
       });
   }
 
   render() {
-
-    const UserProfile = () => {
-      return this.state.userData ? (
-        <Form className="mt-2">
-          <div className="row">
-            <div className="col">
-              <FormGroup check>
-                <Label check>
-                  <Input
-                    type="checkbox"
-                    id="pcp"
-                    checked={this.state.userData.pcpMentor}
-                    onChange={this.handleChange}
-                  />{' '}
-                  PCP Mentor
-                </Label>
-              </FormGroup>
-            </div>
-          </div>
-        </Form>
-      ) : (
-        <h4 className="mt-2">Loading...</h4>
-      );
-
-    }
-
-    return (
+    
+    return this.state.userData ? (
       <div className="page user-profile-view mt-2">
         <div className="row align-items-center">
           <div className="col">
@@ -88,8 +79,51 @@ class UserProfilePage extends React.Component<IUserProfileProps, IUserProfileSta
           </div>
         </div>
         <div className="divider" />
-        <UserProfile />
+          <Form className="mt-2">
+            <div className="row">
+              <div className="col">
+                <FormGroup>
+                  <Label for="major">Major</Label>
+                  <Input 
+                    type="text" 
+                    name="major" 
+                    id="major" 
+                    placeholder="Please add major" 
+                    value={this.state.userData.major} 
+                    onChange={this.handleChange("major")}
+                  />
+                </FormGroup>
+                <FormGroup check>
+                  <Label check>
+                    <Input
+                      type="checkbox"
+                      id="pcp"
+                      checked={this.state.userData.pcpMentor}
+                      onChange={this.handleChange("pcpMentor")}
+                    />{' '}
+                    PCP Mentor
+                  </Label>
+                </FormGroup>
+                <br />
+                <FormGroup>
+                  <Label for="preferredComm">Preferred Channel of Communication (For PCP Mentors)</Label>
+                  <Input 
+                    type="text" 
+                    name="preferredComm" 
+                    id="preferredComm" 
+                    placeholder="Please add preferred mode of communication" 
+                    value={this.state.userData.preferredChannel} 
+                    onChange={this.handleChange("preferredChannel")} 
+                  />
+                </FormGroup>
+                <br />
+                <Button onClick={this.handleSave} color="success">Save</Button>
+              </div>
+            </div>
+          </Form>
       </div>
+    ) : (
+      <h4 className="mt-2">Loading...</h4>
     );
   }
 }
